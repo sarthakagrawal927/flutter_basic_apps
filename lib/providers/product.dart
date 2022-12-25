7759b8a9f2a91b3fb7a6741af669f5a2572aee48
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2022-12-25 17:53:24 +0530

    Add favoritism

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +19,21 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
-    isFavorite = !isFavorite;
+  void _setFavValue(bool isFav) {
+    isFavorite = isFav;
     notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final updateUrl = Uri.parse(
+        'https://shop-app-test-fc197-default-rtdb.firebaseio.com/products/${id}.json');
+    final oldStatus = isFavorite;
+    try {
+      _setFavValue(!isFavorite);
+      await http.patch(updateUrl,
+          body: json.encode({'isFavorite': isFavorite}));
+    } catch (err) {
+      _setFavValue(oldStatus);
+    }
   }
 }
